70abd101aeced5d334bbb27391c95114d90abced
andretecnologia <andre@andretecnologia.com.br>
2021-03-21 02:22:18 -0300

    service test

--- src/test/java/com/company/cliente/controller/ClienteControllerTest.java
+++ src/test/java/com/company/cliente/controller/ClienteControllerTest.java
@@ -1,8 +1,10 @@
 package com.company.cliente.controller;
 
 import com.company.cliente.model.Cliente;
+import com.company.cliente.repository.ClienteRepository;
 import com.company.cliente.service.impl.ClienteServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -25,14 +27,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 class ClienteControllerTest {
 
-    @MockBean
+    @Autowired
     ClienteServiceImpl service;
+
+    @MockBean
+    ClienteRepository repository;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     void clientePOSTControllerTest() throws Exception {
+
+        when(repository.save(any(Cliente.class))).thenReturn(mockCliente());
 
         when(service.salvar(any(Cliente.class))).thenReturn(mockCliente());
 
