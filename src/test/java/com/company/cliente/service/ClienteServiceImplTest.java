09808ec17d98bd69a68bdaa245390943750eb945
andretecnologia <andre@andretecnologia.com.br>
2021-03-21 03:01:28 -0300

    readme

--- src/test/java/com/company/cliente/service/ClienteServiceImplTest.java
+++ src/test/java/com/company/cliente/service/ClienteServiceImplTest.java
@@ -6,8 +6,10 @@ import com.company.cliente.service.impl.ClienteServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -18,6 +20,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class ClienteServiceImplTest {
 
 
